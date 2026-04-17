fd3b7951c5f0a2984606181f515c9ee32f856653
Gabriel Guerra <gabri@gabrimac.local>
2026-04-17 20:54:30 +0200

    debug: mover script de diagnóstico a carpeta public

--- public/test_create.php
+++ public/test_create.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'includes/config/config.php';
-require_once 'includes/classes/Database.php';
+require_once '../includes/config/config.php';
+require_once '../includes/classes/Database.php';
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
